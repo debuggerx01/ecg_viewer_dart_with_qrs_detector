42514f113fd9815def94deeb854c0d5686c25178
debuggerx <dx8917312@163.com>
2021-02-16 20:56:47 +0800

    Reformat code.

--- qrs_detector.dart
+++ qrs_detector.dart
@@ -13,7 +13,7 @@ const DETECTION_WINDOW = 40;
 
 const FIND_PEAKS_SPACING = 50;
 
-const refractoryPeriod = 120;
+const REFRACTORY_PERIOD = 120;
 
 const QRS_PEAK_FILTERING_FACTOR = 0.125;
 const NOISE_PEAK_FILTERING_FACTOR = 0.125;
@@ -32,7 +32,7 @@ class QRSDetector {
 
   DoubleLinkedQueue<double> _mostRecentMeasurements = DoubleLinkedQueue();
 
-  void addToMostRecentMeasurements(double val) {
+  void _addToMostRecentMeasurements(double val) {
     _mostRecentMeasurements.add(val);
     if (_mostRecentMeasurements.length > NUMBER_OF_SAMPLES_STORED)
       _mostRecentMeasurements.removeFirst();
@@ -42,13 +42,13 @@ class QRSDetector {
 
   void addPoint(int timeStamp, double point) {
     this.timeStamp = timeStamp;
-    addToMostRecentMeasurements(point);
-    detectPeaks();
+    _addToMostRecentMeasurements(point);
+    _detectPeaks();
   }
 
-  void detectPeaks() async {
+  void _detectPeaks() async {
     /// Measurements filtering - 0-15 Hz band pass filter.
-    List<double> filteredEcgMeasurements = bandPassFilter();
+    List<double> filteredEcgMeasurements = _bandPassFilter();
 
     /// Derivative - provides QRS slope information.
     List<double> differentiatedEcgMeasurements = List.generate(
@@ -64,7 +64,7 @@ class QRSDetector {
     /// Moving-window integration.
     Array integratedEcgMeasurements =
         convolution(Array(squaredEcgMeasurements), ones(INTEGRATION_WINDOW));
-    List<int> detectedPeaksIndices = findPeaks(integratedEcgMeasurements);
+    List<int> detectedPeaksIndices = _findPeaks(integratedEcgMeasurements);
 
     detectedPeaksIndices.removeWhere(
         (ele) => ele < (NUMBER_OF_SAMPLES_STORED - DETECTION_WINDOW));
@@ -73,10 +73,10 @@ class QRSDetector {
         detectedPeaksIndices.length,
         (index) => integratedEcgMeasurements[detectedPeaksIndices[index]]);
 
-    detectQrs(detectedPeaksValues);
+    _detectQrs(detectedPeaksValues);
   }
 
-  List<double> bandPassFilter() {
+  List<double> _bandPassFilter() {
     double nyquistFreq = signalFrequency / 2;
     Butterworth butter = Butterworth();
     butter.lowPass(1, 2, FILTER_LOW_CUT / nyquistFreq);
@@ -85,7 +85,7 @@ class QRSDetector {
         (index) => butter.filter(_mostRecentMeasurements.elementAt(index)));
   }
 
-  List<int> findPeaks(Array data) {
+  List<int> _findPeaks(Array data) {
     int len = data.length;
     List<double> x = List.generate(len + 2 * FIND_PEAKS_SPACING, (index) {
       if (index < FIND_PEAKS_SPACING) return data.first - 1e-6;
@@ -117,29 +117,27 @@ class QRSDetector {
     return ind;
   }
 
-  int detectedQrs = 0;
-  double qrsPeakValue = 0.0;
-  double noisePeakValue = 0.0;
-  int samplesSinceLastDetectedQrs = 0;
-  double thresholdValue = 0.0;
+  double _qrsPeakValue = 0.0;
+  double _noisePeakValue = 0.0;
+  int _samplesSinceLastDetectedQrs = 0;
+  double _thresholdValue = 0.0;
 
-  void detectQrs(List<double> detectedPeaksValues) {
-    samplesSinceLastDetectedQrs += 1;
-    if (samplesSinceLastDetectedQrs > refractoryPeriod) {
+  void _detectQrs(List<double> detectedPeaksValues) {
+    _samplesSinceLastDetectedQrs += 1;
+    if (_samplesSinceLastDetectedQrs > REFRACTORY_PERIOD) {
       if (detectedPeaksValues.length > 0) {
-        if (detectedPeaksValues.last > thresholdValue) {
+        if (detectedPeaksValues.last > _thresholdValue) {
           handleDetection?.call();
-          samplesSinceLastDetectedQrs = 0;
-          detectedQrs = 1;
-          qrsPeakValue = QRS_PEAK_FILTERING_FACTOR * detectedPeaksValues.last +
-              (1 - QRS_PEAK_FILTERING_FACTOR) * qrsPeakValue;
+          _samplesSinceLastDetectedQrs = 0;
+          _qrsPeakValue = QRS_PEAK_FILTERING_FACTOR * detectedPeaksValues.last +
+              (1 - QRS_PEAK_FILTERING_FACTOR) * _qrsPeakValue;
         } else {
-          noisePeakValue =
+          _noisePeakValue =
               NOISE_PEAK_FILTERING_FACTOR * detectedPeaksValues.last +
-                  (1 - NOISE_PEAK_FILTERING_FACTOR) * noisePeakValue;
+                  (1 - NOISE_PEAK_FILTERING_FACTOR) * _noisePeakValue;
         }
-        thresholdValue = noisePeakValue +
-            QRS_NOISE_DIFF_WEIGHT * (qrsPeakValue - noisePeakValue);
+        _thresholdValue = _noisePeakValue +
+            QRS_NOISE_DIFF_WEIGHT * (_qrsPeakValue - _noisePeakValue);
       }
     }
   }
