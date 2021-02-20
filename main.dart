8a420e59848dfe01e0e54e0b04aedeca81d60223
debuggerx <dx8917312@163.com>
2021-02-20 14:20:34 +0800

    handle multi websocket client at same time.

--- main.dart
+++ main.dart
@@ -1,9 +1,8 @@
+import 'dart:async';
 import 'dart:io';
 import 'qrs_detector.dart';
 
 const SAMPLING_RATE = 250;
-
-/// https://www.zealseeker.com/archives/find-peak/
 
 Future sendData(WebSocket webSocket) {
   QRSDetector detector = QRSDetector(
@@ -14,19 +13,25 @@ Future sendData(WebSocket webSocket) {
 
   var file = File('data.txt');
   var timeStamp = 0;
+  var lineIndex = 0;
   return file.readAsLines().then((value) {
-    return value.forEach((ele) {
-      webSocket.add(ele);
+    Timer.periodic(Duration(milliseconds: 1000 ~/ SAMPLING_RATE), (timer) {
+      if (webSocket.closeCode != null || lineIndex > value.length)
+        return timer.cancel();
+
+      webSocket.add(value[lineIndex]);
       timeStamp += 4000;
-      detector.addPoint(timeStamp, double.tryParse(ele));
-      sleep(Duration(milliseconds: 1000 ~/ SAMPLING_RATE));
+      detector.addPoint(timeStamp, double.tryParse(value[lineIndex]));
+      lineIndex++;
     });
   });
 }
 
 main() async {
   HttpServer server = await HttpServer.bind('127.0.0.1', 9988);
-  server.transform(new WebSocketTransformer()).listen((WebSocket webSocket) {
+  server
+      .transform(new WebSocketTransformer())
+      .listen((WebSocket webSocket) async {
     print('WebSocket opened.');
     webSocket.listen(print, onDone: () {
       print('WebSocket closed.');
